f6e988865ba8c1bb5d5c17b30c27b4a30edfffce
metalchocobo <dictgayanilo@gmail.com>
2018-02-13 19:18:42 +0800

    Edited something

--- app/src/main/java/com/example/dictgayanilo/e_commerce/Accounts.java
+++ app/src/main/java/com/example/dictgayanilo/e_commerce/Accounts.java
@@ -3,12 +3,12 @@ package com.example.dictgayanilo.e_commerce;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class register extends AppCompatActivity {
+public class Accounts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_accounts);
     }
 
 }
